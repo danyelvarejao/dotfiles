843c8cc6c76a0dedcc365d0ec386fce93b6cad75
danyelvarejao <danyelvarejao@gmail.com>
2022-09-23 09:52:14 -0300

    quit nvim tree after open file

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -172,7 +172,7 @@ nvim_tree.setup {
       },
     },
     open_file = {
-      quit_on_open = false,
+      quit_on_open = true,
       resize_window = true,
       window_picker = {
         enable = true,
