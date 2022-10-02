6276075850b4c3b1cdbfeba6887e31477d21449f
danyelvarejao <danyelvarejao@gmail.com>
2022-10-02 08:50:40 -0300

    change timeout of highlight yank

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -6,7 +6,7 @@ augroup('YankHighlight', { clear = true })
 autocmd('TextYankPost', {
   group = 'YankHighlight',
   callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '200' })
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '100' })
   end
 })
 
