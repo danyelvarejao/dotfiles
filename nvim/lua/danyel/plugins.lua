e51f377fd43c6163d47bf278b2b5dd0fb687411b
danyelvarejao <danyelvarejao@gmail.com>
2022-11-22 08:56:41 -0300

    update theme to tokyodark

--- nvim/lua/danyel/plugins.lua
+++ nvim/lua/danyel/plugins.lua
@@ -63,7 +63,7 @@ return packer.startup(function(use)
   use 'nvim-telescope/telescope.nvim'
 
   -- Color scheme
-  use 'navarasu/onedark.nvim'
+  use 'tiagovla/tokyodark.nvim'
 
   -- Statusline
   use 'nvim-lualine/lualine.nvim'
