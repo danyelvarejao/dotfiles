534fe08e5a4765a70135712b3a16812e8487aed2
danyelvarejao <danyelvarejao@gmail.com>
2022-11-23 08:55:59 -0300

    fix error in treesitter

--- nvim/lua/danyel/plugins.lua
+++ nvim/lua/danyel/plugins.lua
@@ -43,7 +43,13 @@ return packer.startup(function(use)
   use 'kyazdani42/nvim-web-devicons'
 
   -- Syntax Highlighting
-  use 'nvim-treesitter/nvim-treesitter'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
 
   -- LSP (Language Servers Protocol)
   use 'neovim/nvim-lspconfig'
