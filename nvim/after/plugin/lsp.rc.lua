9c6e1774796e67ff47b8233a6c5a1ac95d4d7e11
danyelvarejao <danyelvarejao@gmail.com>
2022-11-27 22:52:46 -0300

    add highlight all occurrences on cursor hold LSP

--- nvim/after/plugin/lsp.rc.lua
+++ nvim/after/plugin/lsp.rc.lua
@@ -8,6 +8,10 @@ if not cmp_nvim_lsp_status_ok then
   return
 end
 
+-- Update capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
@@ -19,6 +23,26 @@ vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  -- Highlighting references.
+  -- See: https://sbulav.github.io/til/til-neovim-highlight-references/
+  -- for the highlight trigger time see: `vim.opt.updatetime`
+  if client.server_capabilities.documentHighlightProvider then
+    vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+    vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+    vim.api.nvim_create_autocmd("CursorHold", {
+      callback = vim.lsp.buf.document_highlight,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+      desc = "Document Highlight",
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      callback = vim.lsp.buf.clear_references,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+      desc = "Clear All the References",
+    })
+  end
+
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
@@ -42,9 +66,12 @@ local on_attach = function(client, bufnr)
   -- vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
--- Update capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+-- Define `root_dir` when needed
+-- See: https://github.com/neovim/nvim-lspconfig/issues/320
+-- This is a workaround, maybe not work with some servers.
+local root_dir = function()
+  return vim.fn.getcwd()
+end
 
 local servers = {
   'bashls',
@@ -59,11 +86,11 @@ local servers = {
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
+    root_dir = root_dir,
     capabilities = capabilities,
     flags = {
       debounce_text_changes = 150,
-    },
-    root_dir = function() return vim.loop.cwd() end
+    }
   }
 end
 
