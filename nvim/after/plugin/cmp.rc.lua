4370363878e329107ba06d76111816c3c70d33cb
danyelvarejao <danyelvarejao@gmail.com>
2022-12-08 22:06:00 -0300

    disable tab in autocomplete

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -29,6 +29,7 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     -- Tab mapping
+--[[
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -47,6 +48,7 @@ cmp.setup({
         fallback()
       end
     end
+]]
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
