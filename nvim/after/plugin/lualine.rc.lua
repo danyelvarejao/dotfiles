8511415027c7febf784f3fd10760271c83f6433c
danyelvarejao <danyelvarejao@gmail.com>
2023-03-20 02:04:22 -0300

    update(nvim): update lualine theme

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -3,10 +3,65 @@ if not status_ok then
   return
 end
 
-lualine.setup {
+local colors = {
+  black        = '#202020',
+  neon         = '#DFFF00',
+  white        = '#FFFFFF',
+  green        = '#00D700',
+  purple       = '#5F005F',
+  blue         = '#00DFFF',
+  darkblue     = '#00005F',
+  navyblue     = '#000080',
+  brightgreen  = '#9CFFD3',
+  gray         = '#444444',
+  darkgray     = '#3c3836',
+  lightgray    = '#504945',
+  inactivegray = '#7c6f64',
+  orange       = '#FFAF00',
+  red          = '#5F0000',
+  brightorange = '#C08A20',
+  brightred    = '#AF0000',
+  cyan         = '#00DFFF',
+  transparent  = 'transparent',
+}
+
+theme = {
+  normal = {
+    a = { bg = colors.neon, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.gray, fg = colors.white },
+    c = { bg = colors.black, fg = colors.brightgreen },
+  },
+  insert = {
+    a = { bg = colors.blue, fg = colors.darkblue, gui = 'bold' },
+    b = { bg = colors.navyblue, fg = colors.white },
+    c = { bg = colors.purple, fg = colors.white },
+  },
+  visual = {
+    a = { bg = colors.orange, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.white },
+    c = { bg = colors.red, fg = colors.white },
+  },
+  replace = {
+    a = { bg = colors.brightred, fg = colors.white, gui = 'bold' },
+    b = { bg = colors.cyan, fg = colors.darkblue },
+    c = { bg = colors.navyblue, fg = colors.white },
+  },
+  command = {
+    a = { bg = colors.green, fg = colors.black, gui = 'bold' },
+    b = { bg = colors.darkgray, fg = colors.white },
+    c = { bg = colors.black, fg = colors.brightgreen },
+  },
+  inactive = {
+    a = { bg = colors.transparent, fg = colors.gray, gui = 'bold' },
+    b = { bg = colors.transparent, fg = colors.gray },
+    c = { bg = colors.transparent, fg = colors.gray },
+  },
+}
+
+lualine.setup({
   options = {
     icons_enabled = true,
-    theme = 'powerline_dark',
+    theme = theme,
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
@@ -42,4 +97,4 @@ lualine.setup {
   winbar = {},
   inactive_winbar = {},
   extensions = {}
-}
+})
