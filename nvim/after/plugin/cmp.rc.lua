4bee7bd3a0706c64423afb928ef5b1562b2ccb18
danyelvarejao <danyelvarejao@gmail.com>
2023-07-24 03:21:42 -0300

    fix(nvim): fix lspkind configs

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -39,11 +39,16 @@ cmp.setup({
 
   -- LSPKind to icons in autocomplete
   formatting = {
-    format = function(entry, vim_item)
-      -- Max 30 characters of autocomplete item name
-      vim_item.abbr = string.sub(vim_item.abbr, 1, 30)
-      return vim_item
-    end
+    format = lspkind.cmp_format({
+      maxwidth = 30, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
   }
 })
 
